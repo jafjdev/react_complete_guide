5616a830d299fa7332e1cbe5604ded0660221bcb
José Cedeño <jafjdev@gmail.com>
2020-07-24 15:48:18 -0400

    [148] Created logo & sidedrawer.

--- project/src/components/Layout/Layout.js
+++ project/src/components/Layout/Layout.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Aux from "../../hoc/Auxiliary";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
 import classes from './Layout.css';
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
 const Layout = props => {
 
   return (
     <Aux>
       <Toolbar/>
+      <SideDrawer/>
       <div>Toolbar, Layout, etc</div>
       <main className={classes.Content}>
         {props.children}
