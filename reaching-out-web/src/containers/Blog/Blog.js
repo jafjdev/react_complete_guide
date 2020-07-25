fd2848995b45e68ff2cf1649ff076f4f5b3aa52d
José Cedeño <jafjdev@gmail.com>
2020-07-25 01:13:25 -0400

    [164] Consuming and transforming data.

--- reaching-out-web/src/containers/Blog/Blog.js
+++ reaching-out-web/src/containers/Blog/Blog.js
@@ -1,28 +1,46 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
+import axios from 'axios';
 import './Blog.css';
 
 class Blog extends Component {
-    render () {
-        return (
-            <div>
-                <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
-                </section>
-                <section>
-                    <FullPost />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
-            </div>
-        );
-    }
+  state = {
+    post: []
+  };
+
+  async componentDidMount() {
+    const post = await axios.get('https://jsonplaceholder.typicode.com/posts');
+    let updatedPost = post.data.slice(0,4);
+    updatedPost = updatedPost.map(post=>{
+      return {
+        ...post,
+        author: 'José Cedeño'
+      }
+    });
+    this.setState({post: updatedPost});
+    console.log('[Blog.js] component did mount ');
+    console.log(this.state.post);
+  }
+
+  render() {
+    const post = this.state.post.map(post => <Post key={post.id} title={post.title} author ={post.author}/>);
+    return (
+      <div>
+        <section className="Posts">
+          {post}
+        </section>
+        <section>
+          <FullPost/>
+        </section>
+        <section>
+          <NewPost/>
+        </section>
+      </div>
+    );
+  }
 }
 
 export default Blog;
