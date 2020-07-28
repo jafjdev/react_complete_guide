1014218323426fef6a1346e090aa76a66912ad92
José Cedeño <jafjdev@gmail.com>
2020-07-27 23:56:01 -0400

    [181] Post method implemented in project.

--- project/src/containers/BurgerBuilder/BurgerBuilder.js
+++ project/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from "../../components/Burger/Burger";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
+import axios from '../../axios-orders'
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -74,7 +75,24 @@ class BurgerBuilder extends Component {
   };
 
   purchaseContinueHandler = () => {
-    alert('You continued!');
+    //alert('You continued!');
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice,
+      customer: {
+        name: 'jose',
+        address: {
+          street: 'test',
+          zipCode: '4123',
+          country: 'Venezuela'
+        },
+        email: 'jose@gmail.com'
+      },
+      deliveryMethod: 'fastest'
+    };
+    axios.post('/orders.json', order).then(r => {
+      console.log(r)
+    }).catch(error => console.log(error))
   };
 
   render() {
