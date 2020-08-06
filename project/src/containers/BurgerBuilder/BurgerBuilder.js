41a40f7dac67da96a196308b886611aa332f1c82
José Cedeño <jafjdev@gmail.com>
2020-08-06 00:58:28 -0400

    [227] Order form working and passing data between pages..

--- project/src/containers/BurgerBuilder/BurgerBuilder.js
+++ project/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -83,30 +83,11 @@ class BurgerBuilder extends Component {
   };
 
   purchaseContinueHandler = () => {
-    //alert('You continued!');
-    /*this.setState({loading: true});
-    const order = {
-      ingredients: this.state.ingredients,
-      price: this.state.totalPrice,
-      customer: {
-        name: 'jose',
-        address: {
-          street: 'test',
-          zipCode: '4123',
-          country: 'Venezuela'
-        },
-        email: 'jose@gmail.com'
-      },
-      deliveryMethod: 'fastest'
-    };
-    axios.post('/orders.json', order).then(r => {
-      this.setState({loading: false, purchasing: false})
-    }).catch(error => this.setState({loading: false, purchasing: false}))
-    */
     const queryParams = [];
     for (let i in this.state.ingredients) {
       queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
     }
+    queryParams.push('price=' + this.state.totalPrice);
     const queryString = queryParams.join('&');
     this.props.history.push({
       pathname: '/checkout',
