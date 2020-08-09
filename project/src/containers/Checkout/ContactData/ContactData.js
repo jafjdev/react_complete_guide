210404d3aad126b9a8e4ca2b8cb21bb891c28f60
José Cedeño <jafjdev@gmail.com>
2020-08-08 21:46:48 -0400

    [250] Endind form module....

--- project/src/containers/Checkout/ContactData/ContactData.js
+++ project/src/containers/Checkout/ContactData/ContactData.js
@@ -14,7 +14,10 @@ class ContactData extends Component {
           type: 'text',
           placeholder: 'Your Name'
         },
-        value: ''
+        value: '',
+        validation: {required: true},
+        valid: false,
+        touched: false
       },
       street: {
         elementType: 'input',
@@ -22,7 +25,10 @@ class ContactData extends Component {
           type: 'text',
           placeholder: 'Street'
         },
-        value: ''
+        value: '',
+        validation: {required: true},
+        valid: false,
+        touched: false
       },
       zipCode: {
         elementType: 'input',
@@ -30,7 +36,14 @@ class ContactData extends Component {
           type: 'text',
           placeholder: 'Zip Code'
         },
-        value: ''
+        value: '',
+        validation: {
+          required: true,
+          minLength: 5,
+          maxLength: 5
+        },
+        valid: false,
+        touched: false
       },
       country: {
         elementType: 'input',
@@ -38,7 +51,10 @@ class ContactData extends Component {
           type: 'text',
           placeholder: 'Country'
         },
-        value: ''
+        value: '',
+        validation: {required: true},
+        valid: false,
+        touched: false
       },
       email: {
         elementType: 'input',
@@ -46,7 +62,10 @@ class ContactData extends Component {
           type: 'email',
           placeholder: 'Your Email'
         },
-        value: ''
+        value: '',
+        validation: {required: true},
+        valid: false,
+        touched: false
       },
       deliveryMethod: {
         elementType: 'select',
@@ -56,10 +75,27 @@ class ContactData extends Component {
             {value: 'cheapest', displayValue: 'Cheapest'},
           ]
         },
-        value: ''
+        value: 'fastest',
+        validation: {},
+        valid: true
       },
     },
-    loading: false
+    loading: false,
+    formIsValid: false
+  };
+
+  checkValidity(value, rules) {
+    let isValid = true;
+    if (rules.required) {
+      isValid = value.trim() !== '' && isValid;
+    }
+    if (rules.minLength) {
+      isValid = value.length >= rules.minLength && isValid;
+    }
+    if (rules.maxLength) {
+      isValid = value.length <= rules.maxLength && isValid;
+    }
+    return isValid;
   };
 
   orderHandler = (event) => {
@@ -78,15 +114,55 @@ class ContactData extends Component {
     console.log(this.props.ingredients);
   };
 
-  render() {
+  inputChangedHandler = (event, inputIdentifier) => {
+    const updatedOrderForm = {
+      ...this.state.orderForm
+    }
+    const updatedFormElement = {
+      ...updatedOrderForm[inputIdentifier]
+    };
 
+    updatedFormElement.value = event.target.value;
+    updatedFormElement.valid = this
+      .checkValidity(
+        updatedFormElement.value,
+        updatedFormElement.validation
+      );
+    updatedFormElement.touched = true;
+    updatedOrderForm[inputIdentifier] = updatedFormElement;
+
+    let formIsValid = true;
+    for (let inputIdentifier in updatedOrderForm) {
+      formIsValid = updatedOrderForm[inputIdentifier].valid && formIsValid;
+    }
+    this.setState({orderForm: updatedOrderForm, formIsValid: formIsValid});
+  };
+
+  render() {
+    const formElementsArray = [];
+    for (let key in this.state.orderForm) {
+      formElementsArray.push({
+        id: key,
+        config: this.state.orderForm[key]
+      })
+      ;
+    }
     let form = (
-      <form>
-        <Input elementType="" elementConfig="" value=""/>
-        <Input className={classes.Input} inputtype="input" type="text" name="email" placeholder="Your Email"/>
-        <Input className={classes.Input} inputtype="input" type="text" name="street" placeholder="Street"/>
-        <Input className={classes.Input} inputtype="input" type="text" name="postal" placeholder="Postal Code"/>
-        <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
+      <form onSubmit={this.orderHandler}>
+        {formElementsArray.map(formElement => (
+            <Input
+              key={formElement.id}
+              elementType={formElement.config.elementType}
+              elementConfig={formElement.config.elementConfig}
+              value={formElement.config.value}
+              shouldValidate={formElement.config.validation}
+              invalid={!formElement.config.valid}
+              touched={formElement.config.touched}
+              changed={(event) => this.inputChangedHandler(event, formElement.id)}
+            />
+          )
+        )}
+        <Button btnType="Success" disabled={!this.state.formIsValid}>ORDER</Button>
       </form>
     );
 
