45c5306dbd83ce5a0cbd548f4de8de1b78807a08
José Cedeno <jafjdev@gmail.com>
2020-08-13 15:38:01 -0400

    decrement, add and subtract with redux

--- redux-practice/src/store/reducer.js
+++ redux-practice/src/store/reducer.js
@@ -1,9 +1,31 @@
 const initialState = {
-  counter: 0
+    counter: 0
 };
 
 const reducer = (state = initialState, action) => {
-  return state;
+    if (action.type === 'INCREMENT') {
+        return {
+            counter: state.counter + 1
+        };
+    }
+    if (action.type === 'DECREMENT') {
+        return {
+            counter: state.counter - 1
+        };
+    }
+
+    if (action.type === 'ADD') {
+        return {
+            counter: state.counter + 5
+        };
+    }
+
+    if (action.type === 'SUBTRACT') {
+        return {
+            counter: state.counter - 5
+        };
+    }
+    return state;
 };
 
 export default reducer;
